9dab21326f4a8fb483e9481801b62a6ee77b5f9c
platinum-otabek <mr.tursinboev@gmail.com>
2020-08-14 05:24:09 +0500

    php3

--- routes/commerce/example/interface/windows-usb.php
+++ routes/commerce/example/interface/windows-usb.php
@@ -17,18 +17,53 @@ use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
  *  copy testfile "\\%COMPUTERNAME%\Receipt Printer"
  *  del testfile
  */
-try {
-    // Enter the share name for your USB printer here
-    $connector = null;
-    //$connector = new WindowsPrintConnector("Receipt Printer");
 
-    /* Print a "Hello world" receipt" */
-    $printer = new Printer($connector);
-    $printer -> text("Hello World!\n");
-    $printer -> cut();
-    
-    /* Close printer */
-    $printer -> close();
-} catch (Exception $e) {
-    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+
+
+function print_check($printing_data){
+
+    try {
+
+        // Enter the share name for your USB printer here
+        //$connector = null;
+        $connector = new WindowsPrintConnector("XP-80C");
+         $printer = new Printer($connector);
+         $printer -> setTextSize(2, 1);
+        foreach($printing_data as $data =>$value ){
+                echo $value->name; echo '   ';
+                echo $value->amount; echo '   ';
+                echo $value->price; echo '   ';
+                echo $value->all_price; echo '   ';
+
+                //$printer -> text($value->name,'  ',$value->amount,'  ',$value->price,'  ',$value->all_price,'');
+
+            }
+        /* Print a "Hello world" receipt" */
+
+        $printer -> text("temir italiya 6%  1.05  700000  735000\n");
+        $printer -> text('Rahmat\n');
+        $printer -> cut();
+        /* Close printer */
+        $printer -> close();
+    } catch (Exception $e) {
+        echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+    }
 }
+$printing_data[] = (object) [
+        'name' => 'temir italiya 6%',
+        'amount' => '1.05',
+        'price' =>'700000',
+        'all_price' =>'735000'
+
+];
+array_push($printing_data,(object) [
+                         'name' => 'temir1 italiya 6%',
+                         'amount' => '1.05',
+                         'price' =>'700000',
+                         'all_price' =>'735000'
+
+                 ]);
+
+print_check($printing_data);
+
+?>
